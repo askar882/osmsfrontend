c4cdb804d19073057381ad8e0e2f4853173c2e45
askar882 <17901680+askar882@users.noreply.github.com>
2022-05-24 09:04:16 +0800

    删除多余页面和模块；汉化settings.js；401改成403。

--- src/settings.js
+++ src/settings.js
@@ -1,35 +1,33 @@
 module.exports = {
-  title: 'OSMS Frontend',
+  title: '智慧云平台运营服务管理系统',
 
   /**
    * @type {boolean} true | false
-   * @description Whether show the settings right-panel
+   * @description 是否显示右侧设置栏。
    */
   showSettings: true,
 
   /**
    * @type {boolean} true | false
-   * @description Whether need tagsView
+   * @description 是否需要TagsView。
    */
   tagsView: true,
 
   /**
    * @type {boolean} true | false
-   * @description Whether fix the header
+   * @description 是否固定顶部导航栏。
    */
   fixedHeader: false,
 
   /**
    * @type {boolean} true | false
-   * @description Whether show the logo in sidebar
+   * @description 是否在侧边菜单栏上显示Logo。
    */
   sidebarLogo: false,
 
   /**
    * @type {string | array} 'production' | ['production', 'development']
-   * @description Need show err logs component.
-   * The default is only used in the production env
-   * If you want to also use it in dev, you can pass ['production', 'development']
+   * @description 是否显示错误日志组件。
    */
-  errorLog: 'production'
+  errorLog: ['production', 'development']
 }
--- src/views/error-log/index.vue
+++ src/views/error-log/index.vue
@@ -1,27 +1,18 @@
 <template>
   <div class="errPage-container">
-    <ErrorA />
-    <ErrorB />
-    <h3>Please click the bug icon in the upper right corner</h3>
+    <h3>请点击右上角BUG小图标</h3>
     <aside>
-      Now the management system are basically the form of the spa, it enhances the user experience, but it also increases the possibility of page problems, a small negligence may lead to the entire page deadlock. Fortunately Vue provides a way to catch handling exceptions, where you can handle errors or report exceptions.
+      现在的管理后台基本都是spa的形式了，它增强了用户体验，但同时也会增加页面出问题的可能性，可能一个小小的疏忽就导致整个页面的死锁。好在Vue官网提供了一个方法来捕获处理异常，你可以在其中进行错误处理或者异常上报。
       <a target="_blank" class="link-type" href="https://panjiachen.github.io/vue-element-admin-site/guide/advanced/error.html">
-        Document introduction
+        文档介绍
       </a>
     </aside>
-    <a href="#">
-      <img src="https://wpimg.wallstcn.com/360e4842-4db5-42d0-b078-f9a84a825546.gif">
-    </a>
   </div>
 </template>
 
 <script>
-import ErrorA from './components/ErrorTestA'
-import ErrorB from './components/ErrorTestB'
-
 export default {
-  name: 'ErrorLog',
-  components: { ErrorA, ErrorB }
+  name: 'ErrorLog'
 }
 </script>
 
