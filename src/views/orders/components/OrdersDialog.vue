517c7b9ea3f9b9b00f1d52a1a718ebbc67baf171
askar882 <17901680+askar882@users.noreply.github.com>
2022-05-26 18:44:37 +0800

    实现订单管理模块的订单添加功能。

---
+++ src/views/orders/components/OrdersDialog.vue
@@ -0,0 +1,287 @@
+<template>
+  <el-dialog
+    :title="(action === 'create' ? '添加' : '修改') + '订单'"
+    :visible.sync="innerVisible"
+    :close-on-click-modal="false"
+    @open="dialogOpen"
+  >
+    <el-form
+      ref="form"
+      label-position="right"
+      label-width="100px"
+      :rules="formRules"
+      :model="formData"
+    >
+      <el-form-item label="客户" prop="customer.id">
+        <el-select
+          v-model="formData.customer.id"
+          :disabled="action === 'edit'"
+          filterable
+        >
+          <el-option
+            v-for="(customer, index) in customers"
+            :key="index"
+            :value="customer.id"
+            :label="customer.name"
+          />
+        </el-select>
+      </el-form-item>
+
+      <el-form-item label="运费" prop="shippingCost">
+        <el-input v-model="formData.shippingCost" type="number" :min="0" />
+      </el-form-item>
+
+      <el-form-item label="地址" prop="address">
+        <el-select
+          v-model="formData.address"
+          :disabled="action === 'edit' || customerAddresses.length < 1"
+          filterable
+        >
+          <el-option
+            v-for="(address, index) in customerAddresses"
+            :key="index"
+            :value="address"
+            :label="address"
+          />
+        </el-select>
+      </el-form-item>
+
+      <el-form-item
+        v-for="(orderItem, index) in formData.orderItems"
+        :key="index"
+        label="产品"
+        :required="action === 'create'"
+      >
+        <el-row :gutter="20" type="flex" justify="space-between">
+          <el-col :span="10">
+            <el-form-item
+              :prop="`orderItems.${index}.product.id`"
+              :rules="formRules.orderItems.product"
+            >
+              <el-select
+                v-model="formData.orderItems[index].product.id"
+                :disabled="action === 'edit'"
+                filterable
+              >
+                <el-option
+                  v-for="(product, productIndex) in filteredProducts"
+                  :key="productIndex"
+                  :value="product.id"
+                  :label="product.name + '(' + product.code + ')'"
+                />
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="4">
+            <el-form-item
+              :prop="`orderItems.${index}.count`"
+              :rules="formRules.orderItems.count"
+            >
+              <el-input
+                v-model="formData.orderItems[index].count"
+                type="number"
+                :min="1"
+                :disabled="action === 'edit'"
+              />
+            </el-form-item>
+          </el-col>
+          <el-col :offset="6" :span="2">
+            <el-button
+              type="primary"
+              size="mini"
+              icon="el-icon-plus"
+              :disabled="action === 'edit' || filteredProducts.length < 1"
+              @click="addOrderItem(index)"
+            />
+          </el-col>
+          <el-col :span="2">
+            <el-button
+              type="primary"
+              size="mini"
+              icon="el-icon-delete"
+              :disabled="formData.orderItems.length < 2 || action === 'edit'"
+              @click="deleteOrderItem(index)"
+            />
+          </el-col>
+        </el-row>
+      </el-form-item>
+    </el-form>
+
+    <div slot="footer" class="dialog-footer">
+      <el-button @click="innerVisible = false">取消</el-button>
+      <el-button
+        type="primary"
+        :loading="submitting"
+        @click="submit"
+      >提交</el-button>
+    </div>
+  </el-dialog>
+</template>
+
+<script>
+import { createOrder, updateOrder } from '@/api/orders'
+import { listCustomers } from '@/api/customers'
+import { deepClone } from '@/utils'
+import { listProducts } from '@/api/products'
+
+export default {
+  props: {
+    visible: {
+      type: Boolean,
+      default: false
+    },
+    action: {
+      type: String,
+      default: 'create',
+      validator: (value) => ['create', 'edit'].includes(value)
+    },
+    data: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+
+  data() {
+    const required = { required: true, message: '此项必填', trigger: 'blur' }
+    return {
+      dummyData: {
+        customer: { id: null },
+        shippingCost: 0,
+        orderItems: [{ product: { id: null }, count: 1 }]
+      },
+      formData: {},
+      formRules: {},
+      editRules: {},
+      createRules: {
+        customer: {
+          id: [required]
+        },
+        shippingCost: [required],
+        address: [required],
+        orderItems: {
+          product: [required],
+          count: [required]
+        }
+      },
+      submitting: false,
+      customers: [],
+      products: []
+    }
+  },
+
+  computed: {
+    innerVisible: {
+      get() {
+        return this.visible
+      },
+      set(val) {
+        this.$emit('update:visible', val)
+      }
+    },
+    filteredProducts() {
+      return this.products.filter(
+        (product) =>
+          this.formData.orderItems.find(
+            (item) => item.product.id === product.id
+          ) === undefined
+      )
+    },
+    customerAddresses() {
+      const customerId = this.formData.customer.id
+      if (![undefined, null, ''].includes(customerId)) {
+        return this.customers.find((customer) => customer.id === customerId)
+          .addresses
+      }
+      return []
+    }
+  },
+
+  created() {
+    this.formRules = this.createRules
+    // 为了复用dummyData
+    this.formData = deepClone(this.dummyData)
+    listCustomers()
+      .then(({ data }) => (this.customers = data.customers))
+      .catch((e) => {
+        console.debug(e)
+        this.$message({
+          message: '获取客户列表失败',
+          type: 'error'
+        })
+      })
+    listProducts()
+      .then(({ data }) => (this.products = data.products))
+      .catch((e) => {
+        console.debug(e)
+        this.$message({
+          message: '获取商品列表失败',
+          type: 'error'
+        })
+      })
+  },
+
+  methods: {
+    dialogOpen() {
+      this.submitting = false
+      this.formData = deepClone(this.dummyData)
+      if (this.action === 'create') {
+        this.formRules = this.createRules
+      } else {
+        this.formRules = this.editRules
+        const { id, customer, ...order } = this.data
+        order.customer = { id: customer.id }
+        this.formData = order
+      }
+      this.$nextTick(() => this.$refs?.form?.clearValidate())
+    },
+    async submit() {
+      try {
+        await this.$refs.form.validate()
+      } catch (e) {
+        console.debug('Form validation failed: ', e)
+        this.$message({
+          message: '表单验证失败',
+          type: 'error'
+        })
+        return false
+      }
+      this.submitting = true
+      let action = this.createData
+      if (this.action === 'edit') {
+        action = this.updateData
+      }
+      try {
+        const data = await action()
+        this.$message({
+          message: '提交成功',
+          type: 'success'
+        })
+        this.innerVisible = false
+        this.$emit('success', data)
+      } catch (e) {
+        console.debug('Submit failed:', e)
+        this.$message({
+          message: '提交失败',
+          type: 'error'
+        })
+      }
+      this.submitting = false
+    },
+    async createData() {
+      return (await createOrder(this.formData)).data.order
+    },
+    async updateData() {
+      return (await updateOrder(this.data.id, this.formData)).data.order
+    },
+    addOrderItem(index) {
+      this.formData.orderItems.splice(index + 1, 0, {
+        product: { id: null },
+        count: 1
+      })
+    },
+    deleteOrderItem(index) {
+      this.formData.orderItems.splice(index, 1)
+    }
+  }
+}
+</script>
--- src/views/orders/index.vue
+++ src/views/orders/index.vue
@@ -1,9 +1,162 @@
 <template>
-  <div>test</div>
+  <div class="container">
+    <div class="controls-container">
+      <el-button
+        type="primary"
+        icon="el-icon-plus"
+        @click="addOrder"
+      >添加</el-button>
+    </div>
+    <el-table
+      :data="tableData"
+      :loading="tableLoading"
+      :height="500"
+      border
+    >
+      <el-table-column prop="id" label="ID" :width="60" />
+      <el-table-column prop="customer.name" label="下单客户" />
+      <el-table-column prop="address" label="地址" />
+      <el-table-column prop="totalCost" label="订单总价" />
+      <el-table-column prop="shippingCost" label="运费" />
+      <el-table-column label="下单时间">
+        <template slot-scope="{ row }">
+          {{ row.orderTime | parseDate }}
+        </template>
+      </el-table-column>
+      <el-table-column label="订单列表">
+        <template slot-scope="{ row }">
+          <el-tree v-if="row.orderItemsTree" :data="row.orderItemsTree" :props="{ children: 'children', label: 'label' }" />
+          <p v-else>{{ row.orderItems[0].product.name }}</p>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="{ row }">
+          <el-button
+            size="mini"
+            type="primary"
+            icon="el-icon-edit"
+            @click="handleEdit(row)"
+          >编辑</el-button>
+          <el-button
+            size="mini"
+            type="danger"
+            icon="el-icon-delete"
+            @click="handleDelete(row)"
+          >删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <orders-dialog
+      :visible.sync="dialogVisible"
+      :data="dialogData"
+      :action="dialogAction"
+      @success="onSubmitSuccess"
+    />
+  </div>
 </template>
 
 <script>
+import OrdersDialog from './components/OrdersDialog'
+import { listOrders, deleteOrder } from '@/api/orders'
+import { parseTime } from '@/utils'
+
 export default {
-  name: 'Orders'
+  name: 'Orders',
+  components: { OrdersDialog },
+  filters: {
+    parseDate: time => parseTime(time, '{y}年{m}月{d}日')
+  },
+  data() {
+    return {
+      tableData: [],
+      dialogVisible: false,
+      dialogData: {},
+      dialogAction: 'create',
+      tableLoading: false
+    }
+  },
+  created() {
+    this.getData()
+  },
+  methods: {
+    async getData() {
+      this.tableLoading = true
+      try {
+        const { orders } = (await listOrders()).data
+        console.debug('orders:', orders)
+        orders.forEach(order => {
+          if (order.orderItems.length > 1) {
+            const root = {
+              label: order.orderItems[0].product.name,
+              children: []
+            }
+            order.orderItems.slice(1).forEach(orderItem => root.children.push({ label: orderItem.product.name }))
+            order.orderItemsTree = [root]
+          }
+        })
+        this.tableData = orders
+      } catch (e) {
+        console.debug('Failed to list orders:', e)
+        this.$message({
+          message: '获取客户列表失败',
+          type: 'error'
+        })
+      }
+      this.tableLoading = false
+    },
+    handleEdit(order) {
+      this.dialogData = order
+      this.dialogAction = 'edit'
+      this.dialogVisible = true
+    },
+    async handleDelete(order) {
+      try {
+        await this.$confirm(`是否确认删除客户'${order.name}'？`, '提示', {
+          confirmButtonClass: 'el-button--danger',
+          type: 'warning'
+        })
+      } catch {
+        return false
+      }
+      this.tableLoading = true
+      try {
+        await deleteOrder(order.id)
+        this.$message({
+          message: '删除成功',
+          type: 'success'
+        })
+        this.getData()
+      } catch (e) {
+        console.debug(`Failed to delete order '${order.name}'. Error:`, e)
+        this.$message({
+          message: '删除失败',
+          type: 'error'
+        })
+      }
+      this.tableLoading = false
+    },
+    addOrder() {
+      this.dialogData = {}
+      this.dialogAction = 'create'
+      this.dialogVisible = true
+    },
+    onSubmitSuccess(data) {
+      console.debug('Submit succeeded:', data)
+      this.getData()
+    }
+  }
 }
 </script>
+
+<style scoped>
+.container {
+  margin: 20px;
+}
+
+.controls-container {
+  margin-bottom: 10px;
+  padding: 0 20px;
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
