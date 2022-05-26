97f7fcbbad63da0243ba8098cad10ab287e5239d
askar882 <17901680+askar882@users.noreply.github.com>
2022-05-26 22:08:21 +0800

    订单列表使用步骤条显示订单状态，实现光标悬停在步骤上时显示完成的时间。固定操作列，调整列宽。

--- src/views/orders/index.vue
+++ src/views/orders/index.vue
@@ -7,29 +7,64 @@
         @click="addOrder"
       >添加</el-button>
     </div>
-    <el-table
-      :data="tableData"
-      :loading="tableLoading"
-      :height="500"
-      border
-    >
-      <el-table-column prop="id" label="ID" :width="60" />
-      <el-table-column prop="customer.name" label="下单客户" />
-      <el-table-column prop="address" label="地址" />
-      <el-table-column prop="totalCost" label="订单总价" />
-      <el-table-column prop="shippingCost" label="运费" />
-      <el-table-column label="下单时间">
+    <el-table :data="tableData" :loading="tableLoading" :height="500" border>
+      <el-table-column prop="id" label="ID" width="60" />
+      <el-table-column prop="customer.name" label="下单客户" width="100" />
+      <el-table-column prop="address" label="地址" width="300" />
+      <el-table-column prop="totalCost" label="总价" width="60" />
+      <el-table-column prop="shippingCost" label="运费" width="60" />
+      <el-table-column label="订单状态" width="300">
         <template slot-scope="{ row }">
-          {{ row.orderTime | parseDate }}
+          <el-steps
+            :space="100"
+            :active="
+              row.deliveryTime !== null ? 3 : row.shipmentTime !== null ? 2 : 1
+            "
+            finish-status="success"
+          >
+            <el-step>
+              <el-tooltip
+                slot="title"
+                placement="top"
+                :content="parseTime(row.orderTime)"
+              >
+                <div>已下单</div>
+              </el-tooltip>
+            </el-step>
+            <el-step>
+              <el-tooltip
+                slot="title"
+                placement="top"
+                :content="parseTime(row.shipmentTime)"
+                :disabled="row.shipmentTime === null"
+              >
+                <div>正在运输</div>
+              </el-tooltip>
+            </el-step>
+            <el-step>
+              <el-tooltip
+                slot="title"
+                placement="top"
+                :content="parseTime(row.deliveryTime)"
+                :disabled="row.deliveryTime === null"
+              >
+                <div>已签收</div>
+              </el-tooltip>
+            </el-step>
+          </el-steps>
         </template>
       </el-table-column>
       <el-table-column label="订单列表">
         <template slot-scope="{ row }">
-          <el-tree v-if="row.orderItemsTree" :data="row.orderItemsTree" :props="{ children: 'children', label: 'label' }" />
+          <el-tree
+            v-if="row.orderItemsTree"
+            :data="row.orderItemsTree"
+            :props="{ children: 'children', label: 'label' }"
+          />
           <p v-else>{{ row.orderItems[0].product.name }}</p>
         </template>
       </el-table-column>
-      <el-table-column label="操作">
+      <el-table-column label="操作" width="200" fixed="right">
         <template slot-scope="{ row }">
           <el-button
             size="mini"
@@ -63,9 +98,6 @@ import { parseTime } from '@/utils'
 export default {
   name: 'Orders',
   components: { OrdersDialog },
-  filters: {
-    parseDate: time => parseTime(time, '{y}年{m}月{d}日')
-  },
   data() {
     return {
       tableData: [],
@@ -84,13 +116,17 @@ export default {
       try {
         const { orders } = (await listOrders()).data
         console.debug('orders:', orders)
-        orders.forEach(order => {
+        orders.forEach((order) => {
           if (order.orderItems.length > 1) {
             const root = {
               label: order.orderItems[0].product.name,
               children: []
             }
-            order.orderItems.slice(1).forEach(orderItem => root.children.push({ label: orderItem.product.name }))
+            order.orderItems
+              .slice(1)
+              .forEach((orderItem) =>
+                root.children.push({ label: orderItem.product.name })
+              )
             order.orderItemsTree = [root]
           }
         })
@@ -143,7 +179,8 @@ export default {
     onSubmitSuccess(data) {
       console.debug('Submit succeeded:', data)
       this.getData()
-    }
+    },
+    parseTime: parseTime
   }
 }
 </script>
