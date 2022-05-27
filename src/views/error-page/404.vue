11468cb94bbbe8f0a443fc80e6c9aadfc8b4ecaa
askar882 <17901680+askar882@users.noreply.github.com>
2022-05-27 12:51:04 +0800

    修改404页面；汉化右侧设置、顶部搜索；本地存储Logo，Logo右侧显示设置的标题。

--- src/views/error-page/404.vue
+++ src/views/error-page/404.vue
@@ -9,12 +9,9 @@
       </div>
       <div class="bullshit">
         <div class="bullshit__oops">OOPS!</div>
-        <div class="bullshit__info">All rights reserved
-          <a style="color:#20a0ff" href="https://wallstreetcn.com" target="_blank">wallstreetcn</a>
-        </div>
         <div class="bullshit__headline">{{ message }}</div>
-        <div class="bullshit__info">Please check that the URL you entered is correct, or click the button below to return to the homepage.</div>
-        <a href="" class="bullshit__return-home">Back to home</a>
+        <div class="bullshit__info">请检查您输入的URL是否正确，也可点击下面的按钮回到首页。</div>
+        <a href="" class="bullshit__return-home">去往首页</a>
       </div>
     </div>
   </div>
@@ -26,7 +23,7 @@ export default {
   name: 'Page404',
   computed: {
     message() {
-      return 'The webmaster said that you can not enter this page...'
+      return '未找到相关页面...'
     }
   }
 }
