bcd8635ea445bd0964dc045caa13eca553bca32f
askar882 <17901680+askar882@users.noreply.github.com>
2022-05-27 16:02:40 +0800

    删除TodoList，存档。

--- src/views/dashboard/components/PanelGroup.vue
+++ src/views/dashboard/components/PanelGroup.vue
@@ -1,54 +1,54 @@
 <template>
   <el-row :gutter="40" class="panel-group">
     <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
-      <div class="card-panel" @click="handleSetLineChartData('newVisitis')">
+      <div class="card-panel" @click="selectionChanged('customer')">
         <div class="card-panel-icon-wrapper icon-people">
           <svg-icon icon-class="peoples" class-name="card-panel-icon" />
         </div>
         <div class="card-panel-description">
           <div class="card-panel-text">
-            New Visits
+            客户
           </div>
-          <count-to :start-val="0" :end-val="102400" :duration="2600" class="card-panel-num" />
+          <count-to :start-val="0" :end-val="dataCount.customer" :duration="2600" class="card-panel-num" />
         </div>
       </div>
     </el-col>
     <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
-      <div class="card-panel" @click="handleSetLineChartData('messages')">
+      <div class="card-panel" @click="selectionChanged('dealer')">
         <div class="card-panel-icon-wrapper icon-message">
-          <svg-icon icon-class="message" class-name="card-panel-icon" />
+          <svg-icon icon-class="dealer" class-name="card-panel-icon" />
         </div>
         <div class="card-panel-description">
           <div class="card-panel-text">
-            Messages
+            经销商
           </div>
-          <count-to :start-val="0" :end-val="81212" :duration="3000" class="card-panel-num" />
+          <count-to :start-val="0" :end-val="dataCount.dealer" :duration="3000" class="card-panel-num" />
         </div>
       </div>
     </el-col>
     <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
-      <div class="card-panel" @click="handleSetLineChartData('purchases')">
+      <div class="card-panel" @click="selectionChanged('product')">
         <div class="card-panel-icon-wrapper icon-money">
-          <svg-icon icon-class="money" class-name="card-panel-icon" />
+          <svg-icon icon-class="product" class-name="card-panel-icon" />
         </div>
         <div class="card-panel-description">
           <div class="card-panel-text">
-            Purchases
+            产品
           </div>
-          <count-to :start-val="0" :end-val="9280" :duration="3200" class="card-panel-num" />
+          <count-to :start-val="0" :end-val="dataCount.product" :duration="3200" class="card-panel-num" />
         </div>
       </div>
     </el-col>
     <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
-      <div class="card-panel" @click="handleSetLineChartData('shoppings')">
+      <div class="card-panel" @click="selectionChanged('order')">
         <div class="card-panel-icon-wrapper icon-shopping">
           <svg-icon icon-class="shopping" class-name="card-panel-icon" />
         </div>
         <div class="card-panel-description">
           <div class="card-panel-text">
-            Shoppings
+            订单
           </div>
-          <count-to :start-val="0" :end-val="13600" :duration="3600" class="card-panel-num" />
+          <count-to :start-val="0" :end-val="dataCount.order" :duration="3600" class="card-panel-num" />
         </div>
       </div>
     </el-col>
@@ -59,12 +59,16 @@
 import CountTo from 'vue-count-to'
 
 export default {
-  components: {
-    CountTo
+  components: { CountTo },
+  props: {
+    dataCount: {
+      type: Object,
+      default: () => ({ customer: 102400, dealer: 81212, product: 9280, order: 13600 })
+    }
   },
   methods: {
-    handleSetLineChartData(type) {
-      this.$emit('handleSetLineChartData', type)
+    selectionChanged(type) {
+      this.$emit('selectionChanged', type)
     }
   }
 }
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,38 +1,32 @@
 <template>
   <div class="dashboard-editor-container">
-    <panel-group @handleSetLineChartData="handleSetLineChartData" />
+    <panel-group :data-count="dataCount" @selectionChanged="handlePanelSelected" />
 
+    <!-- 月销售额折线图 -->
     <el-row style="background:#fff;padding:16px 16px 0;margin-bottom:32px;">
       <line-chart :chart-data="lineChartData" />
     </el-row>
 
     <el-row :gutter="32">
+      <!-- 经销商市场占比饼图 -->
       <el-col :xs="24" :sm="24" :lg="8">
         <div class="chart-wrapper">
           <raddar-chart />
         </div>
       </el-col>
+
+      <!-- 订单状态分布饼图 -->
       <el-col :xs="24" :sm="24" :lg="8">
         <div class="chart-wrapper">
           <pie-chart />
         </div>
       </el-col>
+
+      <!-- 热销产品排行柱状图 -->
       <el-col :xs="24" :sm="24" :lg="8">
         <div class="chart-wrapper">
           <bar-chart />
         </div>
-      </el-col>
-    </el-row>
-
-    <el-row :gutter="8">
-      <el-col :xs="{span: 24}" :sm="{span: 24}" :md="{span: 24}" :lg="{span: 12}" :xl="{span: 12}" style="padding-right:8px;margin-bottom:30px;">
-        <transaction-table />
-      </el-col>
-      <el-col :xs="{span: 24}" :sm="{span: 12}" :md="{span: 12}" :lg="{span: 6}" :xl="{span: 6}" style="margin-bottom:30px;">
-        <todo-list />
-      </el-col>
-      <el-col :xs="{span: 24}" :sm="{span: 12}" :md="{span: 12}" :lg="{span: 6}" :xl="{span: 6}" style="margin-bottom:30px;">
-        <box-card />
       </el-col>
     </el-row>
   </div>
@@ -44,24 +38,26 @@ import LineChart from './components/LineChart'
 import RaddarChart from './components/RaddarChart'
 import PieChart from './components/PieChart'
 import BarChart from './components/BarChart'
-import TransactionTable from './components/TransactionTable'
-import TodoList from './components/TodoList'
-import BoxCard from './components/BoxCard'
+import { isGuided, guide } from '@/utils/guide'
+import { listDealers } from '@/api/dealers'
+import { listCustomers } from '@/api/customers'
+import { listProducts } from '@/api/products'
+import { listOrders } from '@/api/orders'
 
 const lineChartData = {
-  newVisitis: {
+  customer: {
     expectedData: [100, 120, 161, 134, 105, 160, 165],
     actualData: [120, 82, 91, 154, 162, 140, 145]
   },
-  messages: {
+  dealer: {
     expectedData: [200, 192, 120, 144, 160, 130, 140],
     actualData: [180, 160, 151, 106, 145, 150, 130]
   },
-  purchases: {
+  product: {
     expectedData: [80, 100, 121, 104, 105, 90, 100],
     actualData: [120, 90, 100, 138, 142, 130, 130]
   },
-  shoppings: {
+  order: {
     expectedData: [130, 140, 141, 142, 145, 150, 160],
     actualData: [120, 82, 91, 154, 162, 140, 130]
   }
@@ -74,19 +70,51 @@ export default {
     LineChart,
     RaddarChart,
     PieChart,
-    BarChart,
-    TransactionTable,
-    TodoList,
-    BoxCard
+    BarChart
   },
   data() {
     return {
-      lineChartData: lineChartData.newVisitis
+      lineChartData: lineChartData.customer,
+      dataCount: {},
+      loadingService: this.$loading(),
+      loading: true
+    }
+  },
+  watch: {
+    loading(val) {
+      console.debug('Loading state changed:', val)
+      if (val) {
+        this.loadingService = this.$loading()
+      } else {
+        this.loadingService.close()
+      }
+    }
+  },
+  created() {
+    this.getData()
+  },
+  mounted() {
+    if (!isGuided()) {
+      guide()
     }
   },
   methods: {
-    handleSetLineChartData(type) {
-      this.lineChartData = lineChartData[type]
+    handlePanelSelected(type) {
+      console.debug('handlePanelSelected:', type)
+    },
+    async getData() {
+      this.loading = true
+      const { dealers } = (await listDealers()).data
+      const { customers } = (await listCustomers()).data
+      const { products } = (await listProducts()).data
+      const { orders } = (await listOrders()).data
+      this.dataCount = {
+        dealer: dealers.length,
+        customer: customers.length,
+        product: products.length,
+        order: orders.length
+      }
+      this.loading = false
     }
   }
 }
@@ -109,5 +137,9 @@ export default {
   .chart-wrapper {
     padding: 8px;
   }
+}
+
+.loading {
+
 }
 </style>
